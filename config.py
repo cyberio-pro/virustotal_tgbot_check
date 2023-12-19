7230f897d58259f665cf10f0f3a5d636567a0c63
Stanislav <138219377+Stanlyio@users.noreply.github.com>
2023-12-19 13:40:15 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    VIRUSTOTAL_API = os.environ.get("VIRUSTOTAL_API", "")
-    userid = int(os.environ.get('userid',12))
+    APP_ID = int(os.environ.get("APP_ID", 28501381))
+    API_HASH = os.environ.get("API_HASH", "1ec36e0c2bee22312d163f46299052c2")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6968270929:AAE8S0feUjlpA5LEt2fWJhrtS3moATORqXg")
+    VIRUSTOTAL_API = os.environ.get("VIRUSTOTAL_API", "15a924da16e03c34778116fdbed6c3040adf8cd04e4b12c42eb851c63366386b")
+    userid = int(os.environ.get('userid',1569238601))
